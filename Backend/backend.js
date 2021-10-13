d61157a0025f1d7dca3a683308f43371744116de
rohandutta2710 <rohandutta27102000@gmail.com>
2021-10-13 22:14:55 +0530

    new

--- Backend/backend.js
+++ Backend/backend.js
@@ -2,22 +2,30 @@ const express = require('express');
 const app = express();
 const fs = require("fs")
 const bodyParser = require("body-parser")
+const ports = process.env.PORT || 5000 ;
+// middlewares
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const filePath = "./database.json";
 let jsonData = fs.readFileSync(filePath, "utf-8");
 jsonData = JSON.parse(jsonData);
 
-// app.get("/", (req, res) => {
-//     res.send("hi")
-// })
+app.get("/:api", (req, res) => {
+    if(req.params.api === "rdxcontact"){
+        res.json(jsonData);
+    }
+    else{
+        res.status(404).send("Error 404")
+    }
+})
 let date = new Date();
 app.post("/contact", (req, res) => {
-    let data = {...req.body,date:`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`,time:`${date.getHours()}:${date.getMinutes()}/${date.getSeconds()}`};
+    let data = {...req.body,date:`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`,time:`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`};
     jsonData.push(data);
     fs.writeFileSync(filePath, JSON.stringify(jsonData));
     res.send("<h1> Response is saved. Rohan Dutta will reach you asap.<br><a href='https://rohandutta.netlify.app/'>Back to page</a></h1>");
 })
-app.listen("4000", "192.168.1.109", () => {
+app.listen(ports, () => { 
     console.log("Server is running.")
 })
