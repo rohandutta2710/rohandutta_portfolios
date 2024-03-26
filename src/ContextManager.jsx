218a66ac42df838bbfa48bf1133e421d8fe3c6f8
Rohan Dutta <rohandutta27102000@gmail.com>
2024-03-26 15:58:32 +0530

    4.2

--- src/ContextManager.jsx
+++ src/ContextManager.jsx
@@ -1,10 +1,11 @@
 import React, { createContext, useState } from "react";
 const context = createContext();
 const ContextManager = (props) => {
-    const [contactForm,setContactForm] = useState({})
+    const [contactForm,setContactForm] = useState({});
+     const websiteLink = process.env.REACT_APP_API_URL;
     return (
         <>
-            <context.Provider value={{contactForm,setContactForm}}>
+            <context.Provider value={{contactForm,setContactForm,websiteLink}}>
                 {props.children}
             </context.Provider>
         </>
